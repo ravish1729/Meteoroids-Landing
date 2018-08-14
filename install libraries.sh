b6b5bc67907dcf271d825a496bd284de4e24ecb6
ravish <ravish1729@gmail.com>
2018-08-14 12:16:55 +0530

    plotting done

---
+++ install libraries.sh
@@ -0,0 +1,4 @@
+# Assuming numpy, python, matplotlib is already installed
+sudo apt-get install proj-bin
+sudo apt-get install libgeos-dev
+sudo pip3 install --user https://github.com/matplotlib/basemap/archive/master.zip
